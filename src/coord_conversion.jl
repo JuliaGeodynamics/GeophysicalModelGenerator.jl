8f5daad359804d5994d0093ca74d8e77a1b430d0
Marcel Thielmann <Marcel.Thielmann@uni-bayreuth.de>
2021-05-19 00:28:38 +0200

    added conversion from geo to Cartesian

--- src/coord_conversion.jl
+++ src/coord_conversion.jl
@@ -3,3 +3,23 @@
 # This file contains functions to convert from geographical coordinates to Cartesian coordinates and vice versa.
 #
 # Author: Marcel Thielmann, 05/2021
+
+function ConvertGeo2Cart(inputdata::GeoData)
+
+    rearth = 3670e3
+
+    lon = inputdata.lon.values
+    lat = inputdata.lat.values
+    depth = inputdata.depth.values
+
+    R = rearth+inputdata.depth.values
+    
+    # compute Cartesian coordinates and assign them to a ValueList variable
+    X = ValueList("x","m",R.*cosd(inputdata.lon.values).*cosd(inputdata.lat.values))
+    Y = ValueList("y","m",R.*sind(inputdata.lon.values).*cosd(inputdata.lat.values))
+    Z = ValueList("z","m",R.*sind(inputdata.lat.values))
+
+    # assign all data to the respective struct
+    convdata = CartData(X,Y,Z,inputdata.values) 
+
+return convdata
--- src/data_import.jl
+++ src/data_import.jl
@@ -8,13 +8,9 @@
 # import CSV data using standard library functions
 # here we assume that the data is indeed comma separated and that comments are preceded with a "#"
 
-
-
-
-
-function ReadCSV_LatLon(filename::String)
+function ReadCSV_LatLon(filename::AbstractString,DepthCon::AbstractString)
     # import data from file with coordinates given in lat/lon/depth format and additional data given in additional columns
-    # the idea here is to assign the data to a structure of the type ScatteredPoints
+    # the idea here is to assign the data to a structure of the type GeoData which will then be used for further processing
     data,hdr = readdlm(filename,',', Float64,'\n'; header=true, skipblanks=true, comments=true, comment_char='#')
     
     # initialize array of structures to store the data
@@ -35,9 +31,20 @@ function ReadCSV_LatLon(filename::String)
             varunit = GetVariableUnit(hdr[ifield])# get variable unit
             LatData = ValueList(varname,varunit,data[1:end,ifield])
         elseif occursin("depth",hdr[ifield])
+            # ISSUE: WE DEFINE DEPTH AS NEGATIVE, BUT HOW DO WE SET THAT?
+            # WE COULD ADD A FLAG THAT INDICATES THE DEPTH CONVENTION AND 
+            # TREAT IT ACCORDINGLY
             depth_ind = ifield;
             varname = GetVariableName(hdr[ifield])# get variable name
             varunit = GetVariableUnit(hdr[ifield])# get variable unit
+
+            if cmp(DepthCon,"positive") # if depth is given as positive values, convert to negative
+                DepthData = ValueList(varname,varunit,-1*data[1:end,ifield])
+            elseif cmp(DepthCon,"negative")
+                DepthData = ValueList(varname,varunit,data[1:end,ifield])
+            else # default behaviour assumes that dpeth is negative
+                DepthData = ValueList(varname,varunit,data[1:end,ifield])
+            end
             DepthData = ValueList(varname,varunit,data[1:end,ifield])
         end
     end
