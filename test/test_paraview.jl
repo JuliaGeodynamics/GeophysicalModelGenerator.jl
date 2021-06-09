bc6337f0811fbad97934792b18ac3f517a7bd1d5
Boris Kaus <kaus@uni-mainz.de>
2021-06-09 22:23:55 +0200

    added examples/tests to create outout with topography, 2D planes and 1D lines

--- test/test_paraview.jl
+++ test/test_paraview.jl
@@ -11,13 +11,31 @@ Data_set        =   GeoData(Lat,Lon,Depth,(Depthdata=Data,LonData=Lon))
 outfile         =   Write_Paraview(Data_set, "test_depth3D")
 @test outfile[1]    ==  "test_depth3D.vts"
 
-# Horizontal profile @ 200 km
-#Lon,Lat,Depth   =   LonLatDepthGrid(10:20,30:40,-200km);
-#Data_set2       =   GeoData(Lat,Lon,Depth,(LonData=Lon,))  
-#outfile         =   Write_Paraview(Data_set2, "test2")
+# Horizontal profile @ 10 km height
+Lon,Lat,Depth     =   LonLatDepthGrid(10:20,30:40,10km);
+Depth[2:4,2:4,1] .= 25km     # add some fake topography
+
+Data_set2       =   GeoData(Lat,Lon,Depth,(Topography=Depth,))  
+outfile2       =   Write_Paraview(Data_set2, "test2")
+@test outfile2[1]    ==  "test2.vts"
+
+# Cross sections
+Lon,Lat,Depth   =   LonLatDepthGrid(10:20,35,(-300:25:0)km);
+Data_set3       =   GeoData(Lat,Lon,Depth,(DataSet=Depth,))  
+outfile3        =   Write_Paraview(Data_set3, "test3")
+@test outfile3[1]    ==  "test3.vts"
 
 
+Lon,Lat,Depth   =   LonLatDepthGrid(15,30:40,(-300:25:0)km);
+Data_set4       =   GeoData(Lat,Lon,Depth,(DataSet=Depth,))  
+outfile4        =   Write_Paraview(Data_set4, "test4")
+@test outfile4[1]    ==  "test4.vts"
+
+Lon,Lat,Depth   =   LonLatDepthGrid(15,35,(-300:25:0)km);
+Data_set5       =   GeoData(Lat,Lon,Depth,(DataSet=Depth,))  
+outfile5        =   Write_Paraview(Data_set5, "test5")
+@test outfile5[1]    ==  "test5.vts"
 
 
-
+# To be done: cases with vectors (as we will need to do vector transformation from lat/lon)
 
