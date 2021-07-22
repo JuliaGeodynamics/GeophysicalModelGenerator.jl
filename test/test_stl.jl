050685eac2ef1b2dabdaaf42430e0af8c072f981
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2021-07-22 10:07:55 +0200

    missing file

---
+++ test/test_stl.jl
@@ -0,0 +1,21 @@
+# test STL routines
+using Test, GeophysicalModelGenerator
+
+# Load cat MESH
+mesh    =   load("./test_files/cat.stl")
+X,Y,Z   =   XYZGrid(150:180, -15:2:15, 10:5:60) # Create mesh
+
+# Test IsInsideClosedSTL routine for individual points (note: bit slow)
+Phase = zeros(size(X));
+for i in eachindex(X)
+
+    inside = IsInsideClosedSTL(mesh, [X[i], Y[i], Z[i]]) 
+    if inside   
+        Phase[i] = 1;
+    end
+end
+
+@test Phase[14,6,2] == 1.0
+
+#Data_Cat = CartData(X,Y,Z, (Phase=Phase,))
+#Write_Paraview(Data_Cat,"Data_Cat")
