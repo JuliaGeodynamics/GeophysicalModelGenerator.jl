9c8cf5af33fba6b889f6ad9494b15db3e6a5c7a7
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2021-11-10 11:14:51 +0100

    update GeoParams version & new CartData interface

--- src/data_types.jl
+++ src/data_types.jl
@@ -597,6 +597,24 @@ function Base.show(io::IO, d::CartData)
     println(io,"    fields : $(keys(d.fields))")
 end
 
+"""
+    CartData(xyz::Tuple{Array,Array,Array})
+
+This creates a `CartData` struct if you have a Tuple with 3D coordinates as input.
+# Example 
+```julia
+julia> data = CartData(XYZGrid(-10:10,-5:5,0))
+CartData 
+    size : (21, 11, 1)
+    x    ϵ [ -10.0 km : 10.0 km]
+    y    ϵ [ -5.0 km : 5.0 km]
+    z    ϵ [ 0.0 km : 0.0 km]
+    fields : (:Z,)
+```
+"""
+CartData(xyz::Tuple) = CartData(xyz[1],xyz[2],xyz[3],(Z=xyz[3],))
+
+
 
 """
     Convert2UTMzone(d::CartData, proj::ProjectionPoint)  
