78c5b1802c536f515029cc1a42f903d9ed57fbec
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2021-11-11 09:24:48 +0100

    bugfixes & additional interface for ImportTopo

--- src/GMT_utils.jl
+++ src/GMT_utils.jl
@@ -9,6 +9,11 @@ export ImportTopo
     
 Uses GMT to download the topography of a certain region, specified with limits=[lon_min, lon_max, lat_min, lat_max] 
 
+Note: 
+====
+- latitude values in the southern hemisphere should have a minus sign (e.g., -2.8)
+- longitude values that are "west" should *either* come with a minus sign *or* are defined by values >180
+
 | Dataset             |   Resolution |   Description                                               |
 |:----------------    | ------------ | ----------------------------------------------------------- |
 | "@earth_relief_01s" |	1 arc sec 	 | SRTM tiles (14297 tiles, land only, 60S-60N) [NASA/USGS]    |
@@ -20,7 +25,7 @@ Uses GMT to download the topography of a certain region, specified with limits=[
 | "@earth_relief_03m"	|   3 arc min	 | ETOPO1 after Gaussian spherical filtering (5.6 km fullwidth)|
 | "@earth_relief_04m"	|   4 arc min	 | ETOPO1 after Gaussian spherical filtering (7.5 km fullwidth)|
 | "@earth_relief_05m"	|   5 arc min	 | ETOPO1 after Gaussian spherical filtering (9 km fullwidth)  |
-| "@earth_relief_06m"	|   6 arc min	 | ETOPO1 after Gaussian spherical filtering (10 km fullwidth) |
+| "@earth_relief_06m"	|   6 arc min	 | ETOPO1 after Gaussia30n spherical filtering (10 km fullwidth) |
 | "@earth_relief_10m"	|  10 arc min	 | ETOPO1 after Gaussian spherical filtering (18 km fullwidth) |
 | "@earth_relief_15m"	|  20 arc min	 | ETOPO1 after Gaussian spherical filtering (28 km fullwidth) |
 | "@earth_relief_20m"	|  20 arc min	 | ETOPO1 after Gaussian spherical filtering (37 km fullwidth) |
@@ -48,15 +53,38 @@ julia> Write_Paraview(Topo,"Topo_Alps")
 """
 function ImportTopo(limits; file::String="@earth_relief_01m.grd")
 
+    # Correct if negative values are given (longitude coordinates that are west)
+    ind = findall(limits[1:2] .< 0); 
+    limits[ind] .= 360 .+ limits[ind]; 
+    limits[1:2] = sort(limits[1:2])   
+    
+    # Download topo file  
     G               =   gmtread(file, limits=limits, grid=true);
-    if length(G.x)==size(G.z',1)
-      Lon,Lat,Depth   =   LonLatDepthGrid(G.x[1:end],G.y[1:end],0);
-    else
-      Lon,Lat,Depth   =   LonLatDepthGrid(G.x[1:end-1],G.y[1:end-1],0);
-    end
+
+    # Transfer to GeoData
+    nx,ny           =   size(G.z,2), size(G.z,1)
+    Lon,Lat,Depth   =   LonLatDepthGrid(G.x[1:nx],G.y[1:ny],0);
     Depth[:,:,1]    =   1e-3*G.z';
     Topo            =   GeoData(Lon, Lat, Depth, (Topography=Depth*km,))
     
     return Topo
 
 end
+
+"""
+  ImportTopo(; lat::Vector{2}, lon::Vector{2}, file::String="@earth_relief_01m.grd")
+
+Imports topography (using GMT), by specifying keywords for latitude and longitude ranges
+
+# Example
+=========
+```julia
+julia> Topo = ImportTopo(lat=[30,40], lon=[30, 50] )
+```
+The values can also be defined as tuples:
+```julia
+julia> Topo = ImportTopo(lon=(-50, -40), lat=(-10,-5), file="@earth_relief_30s.grd")
+```
+
+"""
+ImportTopo(; lat=[37,49], lon=[4,20], file::String="@earth_relief_01m.grd") = ImportTopo([lon[1],lon[2], lat[1], lat[2]], file=file)
