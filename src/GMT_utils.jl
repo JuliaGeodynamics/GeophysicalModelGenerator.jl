6cc27907c1739eee6d5b0c4d750e2396532353bd
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2021-11-14 22:27:26 +0100

    Make it work with neg & pos lon

--- src/GMT_utils.jl
+++ src/GMT_utils.jl
@@ -14,23 +14,23 @@ Note:
 - latitude values in the southern hemisphere should have a minus sign (e.g., -2.8)
 - longitude values that are "west" should *either* come with a minus sign *or* are defined by values >180
 
-| Dataset             |   Resolution |   Description                                               |
-|:----------------    | ------------ | ----------------------------------------------------------- |
-| "@earth_relief_01s" |	1 arc sec 	 | SRTM tiles (14297 tiles, land only, 60S-60N) [NASA/USGS]    |
-| "@earth_relief_03s"	|   3 arc sec	 | SRTM tiles (14297 tiles, land only, 60S-60N) [NASA/USGS]    |
-| "@earth_relief_15s"	|  15 arc sec	 | SRTM15+ [David Sandwell, SIO/UCSD]                          |
-| "@earth_relief_30s"	|  30 arc sec	 | SRTM30+ [Becker et al., 2009, SIO/UCSD]                     |
-| "@earth_relief_01m"	|   1 arc min	 | ETOPO1 Ice surface [NEIC/NOAA]                              |
-| "@earth_relief_02m"	|   2 arc min	 | ETOPO2v2 Ice surface [NEIC/NOAA]                            |
-| "@earth_relief_03m"	|   3 arc min	 | ETOPO1 after Gaussian spherical filtering (5.6 km fullwidth)|
-| "@earth_relief_04m"	|   4 arc min	 | ETOPO1 after Gaussian spherical filtering (7.5 km fullwidth)|
-| "@earth_relief_05m"	|   5 arc min	 | ETOPO1 after Gaussian spherical filtering (9 km fullwidth)  |
-| "@earth_relief_06m"	|   6 arc min	 | ETOPO1 after Gaussia30n spherical filtering (10 km fullwidth) |
-| "@earth_relief_10m"	|  10 arc min	 | ETOPO1 after Gaussian spherical filtering (18 km fullwidth) |
-| "@earth_relief_15m"	|  20 arc min	 | ETOPO1 after Gaussian spherical filtering (28 km fullwidth) |
-| "@earth_relief_20m"	|  20 arc min	 | ETOPO1 after Gaussian spherical filtering (37 km fullwidth) |
-| "@earth_relief_30m"	|  30 arc min	 | ETOPO1 after Gaussian spherical filtering (55 km fullwidth) |
-| "@earth_relief_60m"	|  60 arc min	 | ETOPO1 after Gaussian spherical filtering (111 km fullwidth)|
+| Dataset                 |   Resolution |   Description                                               |
+|:----------------        | ------------ | ----------------------------------------------------------- |
+| "@earth\\_relief\\_01s" |	1 arc sec 	 | SRTM tiles (14297 tiles, land only, 60S-60N) [NASA/USGS]    |
+| "@earth\\_relief\\_03s"	|   3 arc sec	 | SRTM tiles (14297 tiles, land only, 60S-60N) [NASA/USGS]    |
+| "@earth\\_relief\\_15s"	|  15 arc sec	 | SRTM15+ [David Sandwell, SIO/UCSD]                          |
+| "@earth\\_relief\\_30s"	|  30 arc sec	 | SRTM30+ [Becker et al., 2009, SIO/UCSD]                     |
+| "@earth\\_relief\\_01m"	|   1 arc min	 | ETOPO1 Ice surface [NEIC/NOAA]                              |
+| "@earth\\_relief\\_02m"	|   2 arc min	 | ETOPO2v2 Ice surface [NEIC/NOAA]                            |
+| "@earth\\_relief\\_03m"	|   3 arc min	 | ETOPO1 after Gaussian spherical filtering (5.6 km fullwidth)|
+| "@earth\\_relief\\_04m"	|   4 arc min	 | ETOPO1 after Gaussian spherical filtering (7.5 km fullwidth)|
+| "@earth\\_relief\\_05m"	|   5 arc min	 | ETOPO1 after Gaussian spherical filtering (9 km fullwidth)  |
+| "@earth\\_relief\\_06m"	|   6 arc min	 | ETOPO1 after Gaussia30n spherical filtering (10 km fullwidth) |
+| "@earth\\_relief\\_10m"	|  10 arc min	 | ETOPO1 after Gaussian spherical filtering (18 km fullwidth) |
+| "@earth\\_relief\\_15m"	|  20 arc min	 | ETOPO1 after Gaussian spherical filtering (28 km fullwidth) |
+| "@earth\\_relief\\_20m"	|  20 arc min	 | ETOPO1 after Gaussian spherical filtering (37 km fullwidth) |
+| "@earth\\_relief\\_30m"	|  30 arc min	 | ETOPO1 after Gaussian spherical filtering (55 km fullwidth) |
+| "@earth\\_relief\\_60m"	|  60 arc min	 | ETOPO1 after Gaussian spherical filtering (111 km fullwidth)|
 
 *Note*: this routine is only available once the GMT.jl package is loaded on the REPL
 
@@ -55,9 +55,12 @@ function ImportTopo(limits; file::String="@earth_relief_01m.grd")
 
     # Correct if negative values are given (longitude coordinates that are west)
     ind = findall(limits[1:2] .< 0); 
-    limits[ind] .= 360 .+ limits[ind]; 
-    limits[1:2] = sort(limits[1:2])   
     
+    if (limits[1] < 0) && (limits[2] < 0)
+      limits[ind] .= 360 .+ limits[ind]; 
+      limits[1:2] = sort(limits[1:2])   
+    end
+
     # Download topo file  
     G               =   gmtread(file, limits=limits, grid=true);
 
