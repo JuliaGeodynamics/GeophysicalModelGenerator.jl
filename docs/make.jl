5991e628b6d54eceb821c9435c20e41217da57b6
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2021-11-15 18:20:27 +0100

    missing comma

--- docs/make.jl
+++ docs/make.jl
@@ -26,7 +26,7 @@ makedocs(;
             "Plot GPS vectors" =>  "man/tutorial_GPS.md",
             "Read UTM data" =>  "man/tutorial_UTM.md",
             "VoteMaps" =>  "man/Tutorial_Votemaps.md",      
-            "Kilometer-scale volcano" =>  "man/tutorial_local_Flegrei.md"
+            "Kilometer-scale volcano" =>  "man/tutorial_local_Flegrei.md",
             "Generating LaMEM model" =>  "man/LaPalma_example.md"
         ],
         "User Guide" => Any[
