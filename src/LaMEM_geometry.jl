c764ec06b075d6927d3de577871f3770c5396fa3
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2021-11-23 22:07:25 +0100

    typo

--- src/LaMEM_geometry.jl
+++ src/LaMEM_geometry.jl
@@ -402,7 +402,7 @@ function Compute_ThermalStructure(Temp, X, Y, Z, s::LinearTemp)
 end
 
 """
-    HalfspaceCooling(Tsurface=0, Tmantle=1350, Age=60, Adiabat=0)
+    HalfspaceCoolingTemp(Tsurface=0, Tmantle=1350, Age=60, Adiabat=0)
     
 Sets a halfspace temperature structure in plate
 
