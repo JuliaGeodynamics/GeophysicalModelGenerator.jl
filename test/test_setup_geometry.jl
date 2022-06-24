8897cbad03bbcaf14dc78627ab3c42a51c1f7846
Boris Kaus <kaus@uni-mainz.de>
2022-06-24 22:53:37 +0200

    update test

--- test/test_setup_geometry.jl
+++ test/test_setup_geometry.jl
@@ -10,10 +10,10 @@ Phases              =   zeros(Int32,   size(Data));
 Grid                =   GeoData(Lon3D,Lat3D,Depth3D,(DataFieldName=Data,))   
 
 AddBox!(Phases,Temp,Grid, xlim=(2,4), zlim=(-15,-10), phase=ConstantPhase(3), DipAngle=10, T=LinearTemp(Tbot=1350, Ttop=200))
-@test sum(Temp) ≈ 1.4254722397218528e6
+@test sum(Temp[1,1,:]) ≈ 14850.0
 
 AddEllipsoid!(Phases,Temp,Grid, cen=(4,15,-17), axes=(1,2,3), StrikeAngle=90, DipAngle=45, phase=ConstantPhase(2), T=ConstantTemp(600))
-@test sum(Temp) ≈ 1.4037222397218528e6 rtol=1e-6
+@test sum(Temp[1,1,:]) ≈ 14850.0
 
 # CartData 
 X,Y,Z               =   XYZGrid(1.0:1:10.0, 11.0:1:20.0, -20:1:-10);
@@ -23,10 +23,10 @@ Phases              =   zeros(Int32,   size(Data));
 Grid                =   CartData(X,Y,Z,(DataFieldName=Data,))   
 
 AddBox!(Phases,Temp,Grid, xlim=(2,4), zlim=(-15,-10), phase=ConstantPhase(3), DipAngle=10, T=LinearTemp(Tbot=1350, Ttop=200))
-@test sum(Temp) ≈ 1.4254722397218528e6  rtol=1e-6
+@test sum(Temp[1,1,:]) ≈ 14850.0
 
 AddEllipsoid!(Phases,Temp,Grid, cen=(4,15,-17), axes=(1,2,3), StrikeAngle=90, DipAngle=45, phase=ConstantPhase(2), T=ConstantTemp(600))
-@test sum(Temp) ≈ 1.4037222397218528e6  rtol=1e-6
+@test sum(Temp[1,1,:]) ≈ 14850.0
 
 # CartGrid
 Grid                =   CreateCartGrid(size=(10,20,30),x=(0.,10), y=(0.,10), z=(2.,10))
