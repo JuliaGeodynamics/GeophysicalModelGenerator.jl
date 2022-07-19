717fca5c80c51fd064c80b8df5e9296497a8c0dc
Arne_Spang <arspang@uni-mainz.de>
2022-07-19 15:22:50 +0200

    Added function to add generic volcano topography

--- test/test_lamem.jl
+++ test/test_lamem.jl
@@ -93,3 +93,11 @@ AddCylinder!(Phases,Temp,Grid, base=(0,0,-5), cap=(3,3,-2), radius=1.5, phase=Co
 @test Phases[54,65,75] == 0
 @test Temp[55,46,45]   == 400.0
 @test Temp[55,45,45]   == 800.0
+
+# test adding generic volcano topography
+Grid = ReadLaMEM_InputFile("test_files/SaltModels.dat");
+Topo = makeVolcTopo(Grid, center=[0.0,0.0], height=0.4, radius=1.5, crater=0.5, base=0.1);
+@test Topo.fields.Topography[13,13] ≈ 0.279583654km
+Topo = makeVolcTopo(Grid, center=[0.0,0.0], height=0.8, radius=0.5, crater=0.0, base=0.4, background=Topo.fields.Topography);
+@test Topo.fields.Topography[13,13] ≈ 0.279583654km
+@test Topo.fields.Topography[16,18] ≈ 0.619722436km
