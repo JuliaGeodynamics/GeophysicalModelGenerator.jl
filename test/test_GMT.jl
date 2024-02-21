4bdccc43c59601e0bd7e94df30a7b42e304fc5b6
Boris Kaus <kaus@uni-mainz.de>
2024-02-21 14:35:58 +0100

    update test

--- test/test_GMT.jl
+++ test/test_GMT.jl
@@ -1,14 +1,11 @@
 using Test
 using GeophysicalModelGenerator, GMT
 
-
-
 Topo = ImportTopo(lat=[30,31], lon=[50, 51] )
 @test sum(Topo.depth.val) ≈ 2773.3734999999997
 
 Topo = ImportTopo([50,51, 30,31]);
 @test sum(Topo.depth.val) ≈ 2773.3734999999997
-
 
 test_fwd =  ImportGeoTIFF("test_files/length_fwd.tif", fieldname=:forward)
 @test  maximum(test_fwd.fields.forward) ≈ 33.17775km
