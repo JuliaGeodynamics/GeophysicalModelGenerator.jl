f80d37750e0ddcaf3377f10a5cdeb0497861d741
Boris Kaus <kaus@uni-mainz.de>
2024-02-29 15:22:36 +0100

    add StableRNGs for test

--- test/test_event_counts.jl
+++ test/test_event_counts.jl
@@ -6,8 +6,9 @@ Grid_cart = CartData(XYZGrid(-20:20,-20:.1:20,-30:30))
 Lon,Lat,Depth = LonLatDepthGrid(-20:20,-20:.1:20,-30:30);
 Grid_geo = GeoData(Lon,Lat,Depth,(;Depth))
 
-using Random
-rng = MersenneTwister(123)
+using StableRNGs
+
+rng = StableRNG(123)
 
 # Create some random point data
 pt = rand(rng, 10_000,3) .- 0.5
@@ -23,20 +24,20 @@ ind = findall(R.< 5);
 
 # test the basic routine
 counts = PointData2NearestGrid(pt[:,1],pt[:,2],pt[:,3], NumValue(Grid_cart.x),NumValue(Grid_cart.y), NumValue(Grid_cart.z); radius_factor=2)
-@test extrema(counts) == (0, 86)
+@test extrema(counts) == (0, 85)
 
 # Test if the grid is on a CartData grid
 Grid_Count = PointData2NearestGrid(pt[:,1],pt[:,2],pt[:,3], Grid_cart; radius_factor=2)
-@test extrema(Grid_Count.fields.Count) == (0, 86)
+@test extrema(Grid_Count.fields.Count) == (0, 85)
 
 # Test in case the EQ data is also specified as CartData
 Grid_Count = PointData2NearestGrid(EQ_cart, Grid_cart; radius_factor=2)
-@test extrema(Grid_Count.fields.Count) == (0, 86)
+@test extrema(Grid_Count.fields.Count) == (0, 85)
 
 # Test if the grid is on a GeoData grid
 Grid_Count = PointData2NearestGrid(pt[:,1],pt[:,2],pt[:,3], Grid_geo; radius_factor=2)
-@test extrema(Grid_Count.fields.Count) == (0, 86)
+@test extrema(Grid_Count.fields.Count) == (0, 85)
 
 # Test in case the EQ data is also specified as GeoData
 Grid_Count = PointData2NearestGrid(EQ_geo, Grid_geo; radius_factor=2)
-@test extrema(Grid_Count.fields.Count) == (0, 86)
+@test extrema(Grid_Count.fields.Count) == (0, 85)
