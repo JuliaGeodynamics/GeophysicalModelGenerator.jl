fe88d4815205f7a3c67f4a4b2f64513ea85106fc
ChristianSchuler <chr-schuler@t-online.de>
2024-02-29 14:37:43 +0100

    move CountMap to event_counts.jl

--- src/event_counts.jl
+++ src/event_counts.jl
@@ -114,3 +114,74 @@ function PointData2NearestGrid(pt_x,pt_y,pt_z, X,Y,Z; radius_factor=1)
     return count
 end
 
+"""
+    DatasetCountMap = CountMap(DataSet::GeoData,field::String,stepslon::Int64,stepslat::Int64)
+
+Counts the ones in a 2D GeoData field that is specified by "field" in a control area which is defined by steplon and steplat.
+steplon is the number of control areas in lon direction and steplat the number of control areas in lat direction
+The counts per control area are normalized by the highest count
+
+
+```julia
+julia> Data_Faults         = GeoData(Lon3D,Lat3D,Faults,(Faults=Faults,))
+GeoData 
+    size      : (375, 208, 1)
+    lon       ϵ [ -9.932408319802885 : 34.93985125012068]
+    lat       ϵ [ 35.086096468211394 : 59.919210145128545]
+    depth     ϵ [ 0.0 : 1.0]
+    fields    : (:Faults,)
+
+julia> steplon  = 125
+julia> steplat  = 70
+julia> countmap = CountMap(Data_Faults,"Faults",steplon,steplat)
+
+GeoData 
+    size      : (124, 69, 1)
+    lon       ϵ [ -9.751471789279 : 34.75891471959677]
+    lat       ϵ [ 35.26604656731949 : 59.73926004602028]
+    depth     ϵ [ 0.0 : 1.0]
+    fields    : (:CountMap,)
+```julia
+
+"""
+function CountMap(DataSet::GeoData,field::String,stepslon::Int64,stepslat::Int64)
+
+    lon      = unique(DataSet.lon.val)
+    lat      = unique(DataSet.lat.val)
+
+    # create new lon/lat arrays which hold the boundaries of the control areas
+    lonstep  = LinRange(lon[1],lon[end],stepslon)
+    latstep  = LinRange(lat[1],lat[end],stepslat)
+    dlon     = abs(lonstep[2]-lonstep[1])
+    dlat     = abs(latstep[2]-latstep[1])
+    loncen   = lonstep[1]+dlon/2:dlon:lonstep[end]-dlon/2
+    latcen   = latstep[1]+dlat/2:dlat:latstep[end]-dlat/2
+    countmap = zeros(length(loncen),length(latcen))
+
+    expr =   Meta.parse(field)
+    if !haskey(DataSet.fields,expr[1])
+        error("The GeoData set does not have the field: $(expr[1])")
+    end
+
+    # count the ones in every control area
+    for i in eachindex(loncen)
+        for j in eachindex(latcen)
+            indi          = findall((lon .>= lonstep[i]) .& (lon .<= lonstep[i+1]))
+            indj          = findall((lat .>= latstep[j]) .& (lat .<= latstep[j+1]))
+            dataint       = DataSet.fields[expr[1]][indi,indj,1]
+            count         = sum(dataint)
+            countmap[i,j] = count
+        end
+    end
+
+    # normalize count in every control area
+    maxcount = maximum(countmap)
+    countmap = countmap ./ maxcount
+
+    # create new GeoData
+    Lon3D,Lat3D, Data = LonLatDepthGrid(loncen,latcen,0);
+    Data[:,:,1]       .= countmap
+    DatasetCountMap   = GeoData(Lon3D,Lat3D,Data,(CountMap=Data,))
+
+    return DatasetCountMap
+end
