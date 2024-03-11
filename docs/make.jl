99b495c8d7dc013db54de77db0a59333c70d4ce3
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2024-03-11 22:06:07 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -117,7 +117,8 @@ makedocs(;
             "Gravity code" => "man/gravity_code.md",
             "Numerical model setups" => "man/geodynamic_setups.md",
             "LaMEM" => "man/lamem.md",
-            "Profile Processing" => "man/profile_processing.md"
+            "Profile Processing" => "man/profile_processing.md",
+            "Movies" => "man/movies.md"
         ],
         "List of functions"  => "man/listfunctions.md",
         "Authors" => "man/authors.md",
