544c60b80c3b423def6b18e66ca45a632f323b78
APiccolo89 <85160533+APiccolo89@users.noreply.github.com>
2024-03-13 15:41:45 +0100

    Warning and error message. The warn message is appearing if Origin is defined. The error message is shown when the origin is  set up wrongly. -> Since the error can be fixed easily I add also the solution: 1) Not defying origin; 2) Defyning origin correctly

--- src/Setup_geometry.jl
+++ src/Setup_geometry.jl
@@ -201,6 +201,13 @@ function AddBox!(Phase, Temp, Grid::AbstractGeneralGrid;                 # requi
         Origin = (xlim[1], ylim[1], zlim[2])  # upper-left corner
     end
 
+    if Origin != nothing & T==McKenzie_subducting_slab 
+        @warn  "McKenzie temperature field requires that Origin  = [xmin,ymin,zmax] of the box."
+        if Origin[1] != xlim[1] | Origin[2] != ylim[1] | Origin[3] != zlim[2]
+            @error  "Origin is not set up correctly. For fixing the problem Origin can be left blank or Origin = [xmin,ymin,zmax] of the box"
+        end
+    end
+
     # Perform rotation of 3D coordinates:
     Xrot = X .- Origin[1];
     Yrot = Y .- Origin[2];
