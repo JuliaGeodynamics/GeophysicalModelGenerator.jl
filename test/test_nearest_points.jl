31d6f1e59064221ee3977ce2c54c5a77d3779c85
Boris Kaus <kaus@uni-mainz.de>
2025-12-24 11:03:07 +0100

    update dependencies; fix test

--- test/test_nearest_points.jl
+++ test/test_nearest_points.jl
@@ -10,11 +10,11 @@ X_pt, Y_pt, Z_pt = xyz_grid(1:0.05:5, 0:0.07:8, 1:0.4:5)
 
 # 1D test
 id_1D = nearest_point_indices(NumValue(c_1D.x), X_pt[:])
-@test sum(id_1D) == 166336
+@test sum(id_1D) == 223765
 
 # 2D test
 id_2D = nearest_point_indices(NumValue(c_2D.x), NumValue(c_2D.y), X_pt[:], Y_pt[:])
-@test sum(id_2D) == 992141
+@test sum(id_2D) == 982826
 
 # 3D test
 id_3D = nearest_point_indices(NumValue(c_2D.x), NumValue(c_2D.y), NumValue(c_2D.z), X_pt[:], Y_pt[:], Z_pt[:])
