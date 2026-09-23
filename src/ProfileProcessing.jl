c3733492429f128551652aabe4a21233aa63ca0b
Marcel Thielmann <thielmann.marcel@gmail.com>
2026-09-23 23:12:00 +0200

    cleanup

--- src/ProfileProcessing.jl
+++ src/ProfileProcessing.jl
@@ -319,9 +319,7 @@ function create_profile_volume!(Profile::ProfileData, VolData::NamedTuple; DimsV
                 cross_tmp = cross_section(VolData[ivol], dims = DimsVolCross, Start = Profile.start_lonlat, End = Profile.end_lonlat, Depth_extent = Depth_extent)        # create the cross section
                 # add new fields to the cross_add structure, which already contains the data from the previous datasets
                 names_fields = String.(keys(cross_tmp.fields))
-                println("fields: $(names_fields)")
-                for ifield in eachindex(names_fields)
-                    println("dataset: $(datasetnames[ivol]), field: $(names_fields[ifield])")                    
+                for ifield in eachindex(names_fields)               
                     name_new_field = datasetnames[ivol] * "_" * names_fields[ifield] # name of new field includes name of dataset
                     cross_add = addfield(cross_add, name_new_field, ustrip.(cross_tmp.fields[ifield])) # Note: we use ustrip here, and thereby remove the values, as the cross-section routine made problems
                 end
